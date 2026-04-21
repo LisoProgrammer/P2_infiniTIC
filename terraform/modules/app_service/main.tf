b6370e92eaddede9ad58987c8eae1ff8e495cb94
Lisandro Zapata <zlisandro5@gmail.com>
2026-04-21 09:36:06 -0500

    feat: implementar módulo reutilizable de App Service usando capa gratuita (F1)

---
+++ terraform/modules/app_service/main.tf
@@ -0,0 +1,22 @@
+resource "random_string" "suffix" {
+  length  = 5
+  special = false
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "plan-${var.env}-${random_string.suffix.result}"
+  location            = var.location
+  resource_group_name = var.rg_name
+
+  os_type  = "Linux"
+  sku_name = "F1" # FREE
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "app-${var.env}-${random_string.suffix.result}"
+  location            = var.location
+  resource_group_name = var.rg_name
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {}
+}
