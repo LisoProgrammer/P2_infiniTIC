bd0358dbc2369395af76d93e0c7ed37e2fba8bf5
Lisandro Zapata <zlisandro5@gmail.com>
2026-05-01 21:49:19 -0500

    Re-estructuracion del proyecto

--- terraform/modules/envs/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-shared"
-    storage_account_name = "stterraform123" # CAMBIAR
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
-}
