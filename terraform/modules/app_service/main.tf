07833b6663311c5e75b4e84ce60e8a56f1ff3e7c
Lisandro Zapata <zlisandro5@gmail.com>
2026-05-17 17:40:55 -0500

    Se cambio de app de servicio a app de almacenamiento

--- terraform/modules/app_service/main.tf
+++ terraform/modules/app_service/main.tf
@@ -1,22 +1,13 @@
-resource "azurerm_service_plan" "plan" {
-  name                = "plan-${var.env}"
-  location            = var.location
-  resource_group_name = var.rg_name
-
-  os_type  = "Linux"
-  sku_name = var.sku_name
-}
-
-resource "azurerm_linux_web_app" "app" {
-  name                = "app-${var.env}-${random_string.suffix.result}"
-  location            = var.location
-  resource_group_name = var.rg_name
-  service_plan_id     = azurerm_service_plan.plan.id
-
-  site_config {}
+resource "azurerm_storage_account" "appstorage" {
+  name                     = "st${var.env}${random_string.suffix.result}"
+  resource_group_name      = var.rg_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 resource "random_string" "suffix" {
   length  = 5
   special = false
+  upper   = false
 }
